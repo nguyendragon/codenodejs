4d8ec268068e405dbbe655333df374b0730e1075
LongK3z <longmobietv@gmail.com>
2022-05-23 16:39:51 +0700

    first commit

---
+++ src/configs/connectDB.js
@@ -0,0 +1,12 @@
+// import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
+
+// local
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'admin',
+    password: 'X9WSd4rOn6WZ80Tn',
+    database: 'yblq_q36clublfxy'
+});
+
+export default connection;
